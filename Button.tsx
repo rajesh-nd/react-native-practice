4b8b42fca2d21f782fabc5c1a5b0eb7e2a4688f1
rajesh-nd <rajesh@navadhiti.com>
2023-07-25 16:23:47 +0530

    create react native basic app

---
+++ Button.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+
+function CustomButton(): JSX.Element {
+  return (
+    <TouchableOpacity>
+      <Text>Click...!</Text>
+    </TouchableOpacity>
+  );
+}
+
+export default CustomButton;
